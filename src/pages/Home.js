02a3a27a445e3558eff2d89dc0e70755346ab5c7
clueking1 <wcoan300@gmail.com>
2020-05-07 17:41:15 -0400

    set up display for randomly generated number and a Next and Like button to find your favorite number

---
+++ src/pages/Home.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Home() {
+
+    return (
+        <div className="displayWrapper">
+            <div className="number">
+                <h3>3</h3>
+            </div>
+            
+            <div className="next">
+                <button>Next</button>
+            </div>
+            
+            <div className="like">
+            <button>Like</button>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Home
