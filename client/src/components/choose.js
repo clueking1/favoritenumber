085297f72fa3b8adefb1835bbd9d6d217defacfa
clueking1 <wcoan300@gmail.com>
2020-05-09 20:36:52 -0400

    next and like change the number

--- client/src/components/choose.js
+++ client/src/components/choose.js
@@ -1,22 +1,59 @@
 import React, { useEffect, useState } from 'react'
-import API from '../utils/API'
+
 
 
 function Game(props) {
-    console.log(props)
+    
+    const [rando, setRando] = useState()
+
+    const allnums = props.nums
+    const weight = props.percs
+    let totalweight=eval(weight.join("+"))
+    const weighed= [] //new array to hold "weighted" fruits
+    let current=0
+
+    useEffect(() => loop())
+
+    function pickFrom() {
+        setRando(weighed[Math.floor(Math.random() * weighed.length)])
+    }
+    
+
+    function loop() {
+        while (current<allnums.length){ 
+            for (let i=0; i<weight[current]; i++)
+                weighed[weighed.length]=allnums[current]
+              
+            current++
+            if (current === allnums.length) {
+                pickFrom()
+                
+            }
+        }
+    }
 
     return (
         <div className="displayWrapper">
             <div className="number">
-            <h3>{}</h3>
+            <h3>{rando}</h3>
             </div>
             
             <div className="next">
-                <button>Next</button>
+                <button 
+                onClick={() => {
+                    current = 0 
+                    loop()}}
+                >
+                Next</button>
             </div>
             
             <div className="like">
-            <button>Like</button>
+                <button
+                onClick={() => {
+                    current = 0 
+                    loop()}}
+                >
+                Like</button>
             </div>
             
         </div>
